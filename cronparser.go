d067040f9b62a3830c2030648cb974a154b20e45
AlexxSap <alexxandrsap@mail.ru>
2025-04-15 12:32:49 +0300

    add signature of main functions

--- cronparser.go
+++ cronparser.go
@@ -2,10 +2,52 @@ package cronparser
 
 import "time"
 
-type CronParser struct {
-	cronString string
+type token struct {
 }
 
-func (crn CronParser) IsMatch(time.Time) (bool, error) {
-	return true, nil
+type result struct {
+	minute     token
+	hour       token
+	dayOfMonth token
+	month      token
+	dayOfWeek  token
+}
+
+type CronParser struct {
+	cronString          string
+	alreadyParsedResult result
+	hasResult           bool
+}
+
+func NewCronParser(str string) CronParser {
+	return CronParser{
+		cronString:          str,
+		alreadyParsedResult: result{},
+		hasResult:           false}
+}
+
+func (crn CronParser) parse() (result, error) {
+	if crn.hasResult {
+		return crn.alreadyParsedResult, nil
+	}
+
+	return result{}, nil
+}
+
+func (res result) isMatch(dateTime time.Time) (bool, error) {
+	return false, nil
+}
+
+func (crn CronParser) IsMatch(dateTime time.Time) (bool, error) {
+	res, err := crn.parse()
+	if err != nil {
+		return false, err
+	}
+
+	isMatch, err := res.isMatch(dateTime)
+	if err != nil {
+		return false, err
+	}
+
+	return isMatch, nil
 }
--- cronparser_test.go
+++ cronparser_test.go
@@ -12,10 +12,11 @@ func TestIsMatch(t *testing.T) {
 		expected   bool
 	}{
 		{"* * * * *", time.Date(2025, 4, 1, 12, 33, 1, 0, time.UTC), true},
+		{"* * * * *", time.Date(2012, 12, 12, 12, 12, 12, 12, time.UTC), true},
 	}
 
 	for _, test := range tests {
-		actual, err := CronParser{test.cronString}.IsMatch(test.date)
+		actual, err := NewCronParser(test.cronString).IsMatch(test.date)
 		if err != nil {
 			t.Errorf("error: %v", err)
 		}
@@ -25,3 +26,9 @@ func TestIsMatch(t *testing.T) {
 		}
 	}
 }
+
+/// TODO список ошибок:
+/// - неверное число токенов
+/// - неверное семантическое значение для токена
+/// - неверный символ в токене
+/// - передана невалидная дата
