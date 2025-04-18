30bb64d2d9b0141ed217ac67f6a3ad46c8c67f18
AlexxSap <alexxandrsap@mail.ru>
2025-04-18 14:52:43 +0300

    add some errors and main func

--- cronparser.go
+++ cronparser.go
@@ -1,8 +1,21 @@
 package cronparser
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
+
+const (
+	ErrorValidationEmptyString = "ErrorValidationEmptyString"
+	ErrorValidationTokensCount = "ErrorValidationTokensCount"
+)
 
 type token struct {
+}
+
+func newToken(str string) (token, error) {
+	return token{}, nil
 }
 
 type result struct {
@@ -26,12 +39,66 @@ func NewCronParser(str string) CronParser {
 		hasResult:           false}
 }
 
-func (crn CronParser) parse() (result, error) {
+func tokenize(str string) (result, error) {
+	if len(str) == 0 {
+		/// TODO добавить тест
+		return result{}, errors.New(ErrorValidationEmptyString)
+	}
+
+	strTokens := strings.Fields(str)
+	if len(strTokens) != 5 {
+		/// TODO добавить тест
+		return result{}, errors.New(ErrorValidationTokensCount)
+	}
+
+	minute, err := newToken(strTokens[0])
+	if err != nil {
+		return result{}, err
+	}
+
+	hour, err := newToken(strTokens[1])
+	if err != nil {
+		return result{}, err
+	}
+
+	dayOfMonth, err := newToken(strTokens[2])
+	if err != nil {
+		return result{}, err
+	}
+
+	month, err := newToken(strTokens[3])
+	if err != nil {
+		return result{}, err
+	}
+
+	dayOfWeek, err := newToken(strTokens[4])
+	if err != nil {
+		return result{}, err
+	}
+
+	return result{
+		minute:     minute,
+		hour:       hour,
+		dayOfMonth: dayOfMonth,
+		month:      month,
+		dayOfWeek:  dayOfWeek,
+	}, nil
+}
+
+func (crn *CronParser) parse() (result, error) {
 	if crn.hasResult {
 		return crn.alreadyParsedResult, nil
 	}
 
-	return result{}, nil
+	parsed, err := tokenize(crn.cronString)
+	if err != nil {
+		return result{}, err
+	}
+
+	crn.alreadyParsedResult = parsed
+	crn.hasResult = true
+
+	return parsed, nil
 }
 
 func (res result) isMatch(dateTime time.Time) (bool, error) {
@@ -39,12 +106,12 @@ func (res result) isMatch(dateTime time.Time) (bool, error) {
 }
 
 func (crn CronParser) IsMatch(dateTime time.Time) (bool, error) {
-	res, err := crn.parse()
+	parsed, err := crn.parse()
 	if err != nil {
 		return false, err
 	}
 
-	isMatch, err := res.isMatch(dateTime)
+	isMatch, err := parsed.isMatch(dateTime)
 	if err != nil {
 		return false, err
 	}
